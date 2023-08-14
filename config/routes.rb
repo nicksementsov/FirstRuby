76ff03ecf50658b47ad4681fe9d791bafa195cad
Nick <nickolaisementsov@gmail.com>
2023-08-14 14:48:37 -0400

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+
+  # root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  root "articles#index"
+
+  resources :articles
+
+end
